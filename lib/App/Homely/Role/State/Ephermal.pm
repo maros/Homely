bbb077f358cb2629571435136491ec03b13e11f8
Maros Kollar <maros@k-1.com>
2014-03-02 22:32:28 +0100

    State base classes and roles

--- lib/App/Homely/Role/State/Ephermal.pm
+++ lib/App/Homely/Role/State/Ephermal.pm
@@ -2,7 +2,7 @@ package App::Homely::Role::State::Ephermal {
     use 5.016;
     
     use Moose::Role;
-    requires qw(get_state max_state_age);
+    requires qw(get_state max_state_age fetch_state);
     
     has 'state_age' => (
         is              => 'rw',
@@ -14,12 +14,26 @@ package App::Homely::Role::State::Ephermal {
         my $orig = shift;
         my $self = shift;
         
-        if ($self->has_state_age) {
-            
+        if ($self->has_state_age
+            && $self->state_age + $self->max_state_age < time) {
+            return $self->fetch_state;
         }
         
         return $self->$orig(@_);
     };
+    
+    after 'fetch_state' => sub {
+        my ($self) = @_;
+        $self->state_age(time);
+        $self->store;
+    };
+    
+    sub init_state {
+        my ($class) = @_;
+        my $self = $class->new;
+        $self->fetch_state();
+        return $self;
+    }
 }
 
 1;
--- lib/App/Homely/State.pm
+++ lib/App/Homely/State.pm
@@ -5,15 +5,40 @@ package App::Homely::State {
     use warnings;
     
     use Moose;
+    use MooseX::Storage;
+    with qw(App::Homely::Role::Singelton);
+    with Storage('format' => 'JSON', 'io' => 'File');
     
     use Log::Any qw($log);
     
-    has 'config' => (
-        is              => 'rw',
-        isa             => 'App::Homely::Config',
-        required        => 1,
-    );
+    sub fetch_state;
     
+    sub get_state {
+        my ($class) = @_;
+        if (blessed $class) {
+            return $class;
+        } else {
+            return $class->load($class->_state_file) || $class->new();
+        }
+    }
+    
+    
+    sub _state_file {
+        my ($class) = @_;
+        return Homely::Core->instance->config_dir->file($class->moniker.'.json')
+    }
+    
+    sub moniker {
+        my ($class) = @_;
+        
+        my $moniker = ref($class) || $class;
+        
+        $moniker =~ s/^\Q__PACKAGE__\E:://;
+        $moniker =~ s/::/_/g;
+        $moniker = lc($moniker);
+        
+        return $moniker;
+    }
 }
 
 1;
