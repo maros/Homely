43b0fd2407925c43fe0b0cdd30626d0a08e111af
Maros Kollar <maros@k-1.com>
2014-03-06 07:06:13 +0100

    storage roles

---
+++ lib/App/Homely/Meta/Attribute/DoNotStore.pm
@@ -0,0 +1,8 @@
+package Homely::Meta::Attribute::DoNotStore {
+    use 5.016;
+    use warnings;
+    
+    use Moose::Role;
+};
+
+1;
---
+++ lib/App/Homely/Role/Storage.pm
@@ -0,0 +1,50 @@
+package App::Homely::Role::Storage {
+    use 5.016;
+    
+    use utf8;
+    
+    use namespace::autoclean;
+    use Moose::Role;
+    requires qw(moniker);
+    
+    use Sereal::Encoder qw(encode_sereal);
+    use Sereal::Decoder qw(decode_sereal);
+    
+    sub storage_filename {
+        my ($self,$identifier) = @_;
+        
+        my $name = $self->moniker;
+        if (defined $identifier) {
+            $name .= '_'.$identifier;
+        }
+        $name .= '.ser';
+        return App::Homely::Core->instance->config_dir->file($name);
+    }
+    
+    sub store {
+        my ($self,$identifier) = @_;
+        
+        my %storage;
+        foreach my $attribute ($self->meta->get_all_attributes) {
+            next
+                if $attribute->does('Homely::Meta::Attribute::DoNotStore');
+            $storage{$attribute->name} = $attribute->get_raw_value($self);
+        }
+        
+        my $encoded = encode_sereal(\%storage);
+        my $file = $self->storage_filename($identifier);
+        $file->spew($encoded);
+    }
+    
+    sub load {
+        my ($self,$identifier) = @_;
+        
+        my $file = $self->storage_filename($identifier);
+        my $encoded = $file->slurp();
+        my $storage = decode_sereal($encoded);
+        
+        return bless($storage,ref($self));
+    }
+}
+
+1;
